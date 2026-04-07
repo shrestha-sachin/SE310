65dcd2a5a74b88986ceb8464c33001faabbb0a90
sumit karki <sumitkarki@sumits-MacBook-Pro-3.local>
2026-04-07 11:01:20 -0500

    Integer Division

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def addition(number1, number2):
   print("We are adding " + str(number1) + " and " + str(number2))
   return number1 + number2
 
+def integer_division(number1, number2):
+  print("We are performing integer division on " + str(number1) + " and " + str(number2))
+  return number1 // number2
+
 
 def calculator():
   print("Calculator by team =  " + team_name)
